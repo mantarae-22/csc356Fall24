93c94210b7045dadb329e5b89a9968f0ab5aa8c1
Rae Crusoe <113061355+mantarae-22@users.noreply.github.com>
2024-09-19 13:15:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mars Tourism</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <!-- here is the header; TODO: add AI image -->
+    <header>
+        <h1>Welcome to my Mars Tourism web application.</h1>
+    </header>
+
+    <!-- here is the main section -->
+    <main>
+        <p>
+            My company is awesome. We provide awesome trips to Mars.
+        </p>
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
